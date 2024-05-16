3e71808672ab11565512aebcc87f7a211279d72a
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-05-16 22:38:43 +0530

    better approach

--- 1768_Merge_Strings_Alternately.java
+++ 1768_Merge_Strings_Alternately.java
@@ -1,3 +1,5 @@
+// solution 1 - 15ms
+
 class Solution {
     public String mergeAlternately(String word1, String word2) {
         int i=0,j=0,len1=word1.length(), len2=word2.length();
@@ -23,6 +25,33 @@ class Solution {
         while(j<len2){
             str.append(word2.charAt(j));
             j++;
+        }
+
+        return str.toString();
+    }
+}
+
+
+// solution 2 - 0ms
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int len1=word1.length(), len2=word2.length();
+
+        int minL = (len1>len2)? len2:len1;
+        
+        StringBuilder str = new StringBuilder("");
+        int i=0;
+        for(;i<minL;i++){
+            str.append(word1.charAt(i));
+            str.append(word2.charAt(i));
+        }
+
+        if(minL==len1){
+            str.append(word2.substring(i,len2));
+        }
+        else{
+            str.append(word1.substring(i,len1));
         }
 
         return str.toString();
