e7caf8ea472f79ea206b572f0c6a54adf6c05220
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-06-15 20:46:07 +0530

    next greater element

---
+++ Stack/496. Next Greater Element I.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        int maxS=-1;
+        for(int i=0;i<nums2.length;i++){
+            if(nums2[i]>maxS){
+                maxS=nums2[i];
+            }
+        }
+
+        int[] next = new int[maxS+1];
+        int p=0;
+
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=nums2.length-1;i>=0;i--){
+            while(!s.isEmpty() && s.peek() <= nums2[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                next[nums2[i]] = -1;
+            }
+            else{
+                next[nums2[i]] = s.peek();
+            }
+            s.push(nums2[i]);
+        }
+         
+        int[] ans = new int[nums1.length];
+
+        for(int i=0;i<nums1.length;i++){
+            ans[i] = next[nums1[i]];
+        }
+        return ans;
+    }
+}
