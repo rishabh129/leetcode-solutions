0aad28739ae98655ab1b14c6f70d46cc16cbc500
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-01 13:47:58 +0530

    3 odds

---
+++ contest2.java
@@ -0,0 +1,105 @@
+// You are given a binary array nums.
+
+You can do the following operation on the array any number of times (possibly zero):
+
+Choose any index i from the array and flip all the elements from index i to the end of the array.
+Flipping an element means changing its value from 0 to 1, and from 1 to 0.
+
+Return the minimum number of operations required to make all elements in nums equal to 1.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,1,0,1]
+
+Output: 4
+
+Explanation:
+We can do the following operations:
+
+Choose the index i = 1. The resulting array will be nums = [0,0,0,1,0].
+Choose the index i = 0. The resulting array will be nums = [1,1,1,0,1].
+Choose the index i = 4. The resulting array will be nums = [1,1,1,0,0].
+Choose the index i = 3. The resulting array will be nums = [1,1,1,1,1].
+Example 2:
+
+Input: nums = [1,0,0,0]
+
+Output: 1
+
+Explanation:
+We can do the following operation:
+
+Choose the index i = 1. The resulting array will be nums = [1,1,1,1].
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 1
+
+// give solution
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                res++;
+                dp[i] = 1;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == 1) {
+                cnt++;
+                res = Math.max(res, cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,0,1};
+        System.out.println(minOperations(nums));
+    }
+
+//wrong output
+
+//correct output - 4
+
+//give correct code
+
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                res++;
+                dp[i] = 1;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] == 1) {
+                cnt++;
+                res = Math.max(res, cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,0,1};
+        System.out.println(minOperations(nums));
+    }
+}
