47e11a7b4f3b0ff7b8b2fe86a1f651f441db666d
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-01 21:16:35 +0530

    stack using queue

---
+++ Queue/225. Implement Stack using Queues.java
@@ -0,0 +1,58 @@
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+        
+    }
+    
+    public void push(int x) {
+        q1.add(x);
+    }
+    
+    public int pop() {
+        int temp=-1;
+        while(!q1.isEmpty()){
+            temp = q1.remove();
+            if(q1.isEmpty()){
+                break;
+            }
+            q2.add(temp);
+        }
+
+        while(!q2.isEmpty()){
+            q1.add(q2.remove());
+        }
+        return temp;
+    }
+    
+    public int top() {
+        int temp=-1;
+        while(!q1.isEmpty()){
+            temp = q1.remove();
+            q2.add(temp);
+        }
+
+        while(!q2.isEmpty()){
+            q1.add(q2.remove());
+        }
+        return temp;
+    }
+    
+    public boolean empty() {
+        if(q1.isEmpty())
+        return true;
+
+        return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
