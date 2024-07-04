29318e7ac30a7d165314c7e54f717ad2cbe7119e
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-04 20:48:01 +0530

    stack using only 1 q

--- Queue/225. Implement Stack using Queues.java
+++ Queue/225. Implement Stack using Queues.java
@@ -1,3 +1,5 @@
+// Solution 1 - using 2 queue
+
 class MyStack {
     Queue<Integer> q1;
     Queue<Integer> q2;
@@ -45,6 +47,44 @@ class MyStack {
         return true;
 
         return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+// Solution 2 - using only one queue
+
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+
+        for(int i=1;i<q.size();i++){
+            q.add(q.remove());
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
     }
 }
 
