986cddf93c3f0dbfac9d7b398612c178b9d2f534
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-05 14:32:35 +0530

    2824 sol2

--- 2824. Count Pairs Whose Sum is Less than Target.cpp
+++ 2824. Count Pairs Whose Sum is Less than Target.cpp
@@ -26,3 +26,40 @@ public:
         return count;
     }
 };
+
+// solution 2 - 2 pointer
+
+class Solution
+{
+public:
+    int countPairs(vector<int> &nums, int target)
+    {
+        int count = 0;
+
+        sort(nums.begin(), nums.end());
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right)
+        {
+
+            if (nums[left] + nums[right] < target)
+            {
+                count += right - left;
+                left++;
+                right = nums.size() - 1;
+            }
+            else
+            {
+                if (right - left == 1)
+                {
+                    break;
+                }
+                right--;
+            }
+        }
+
+        return count;
+    }
+};
