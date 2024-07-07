fede4fe1a2ac65458ea8c78d6f860a9441e0f3d1
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-07 11:28:23 +0530

    water bottles

---
+++ 1518. Water Bottles.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int numWaterBottles(int numBottles, int numExchange)
+    {
+        int sum = 0, empty = 0;
+
+        while (numBottles != 0)
+        {
+            sum += numBottles;
+
+            empty += numBottles;
+            numBottles = empty / numExchange;
+            empty = empty % numExchange;
+        }
+        return sum;
+    }
+};
