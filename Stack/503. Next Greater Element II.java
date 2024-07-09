181e6d72f40c0bc2fb0d0dd97fcee912358e3ded
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-09 12:58:29 +0530

    using only stack and circular array

--- Stack/503. Next Greater Element II.java
+++ Stack/503. Next Greater Element II.java
@@ -39,3 +39,32 @@ class Solution {
         return ans;
     }
 }
+
+// using only stack and circular array
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        
+        int len = nums.length;
+        int[] ans =new int[len];
+
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=(2*len)-1;i>=0;i--){
+
+            while(!s.isEmpty() && s.peek()<=nums[i%len]){
+                s.pop();
+            }
+
+            if(!s.isEmpty()){
+                ans[i%len] = s.peek();
+            }
+            else{
+                ans[i%len]=-1;
+            }
+
+            s.push(nums[i%len]);
+        }
+        return ans;
+    }
+}
