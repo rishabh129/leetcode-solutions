1842481c1b016ee4eb5606c59a236569a9d23834
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-07-11 14:09:41 +0530

    pre order

---
+++ Binary Tree/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> traverse(TreeNode root, List<Integer> tree){
+        if(root == null){
+            return tree;
+        }
+        tree.add(root.val);
+
+        traverse(root.left, tree);
+        traverse(root.right, tree);
+        return tree;
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> tree = new ArrayList<>();
+
+        return traverse(root, tree);
+    }
+}
