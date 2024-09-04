fa52fff7ba122df03cf20dca1b82e50877211518
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-04 15:38:02 +0530

    anagram

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        int[] count1=new int[26];
+        int[] count2=new int[26];
+
+        for(int i=0;i<s.length();i++){
+            count1[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            count2[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(count1[i]!=count2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
