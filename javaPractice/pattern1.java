c767fc5219733a16707a48d00728a41dbd862e63
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-04 13:20:59 +0530

    java

---
+++ javaPractice/pattern1.java
@@ -0,0 +1,49 @@
+// ****
+// *  *
+// *  *
+// ****
+
+//code above pattern in java
+
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 1 || i == 4 || j == 1 || j == 4) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// code pattern for hollow rhombus in java
+
+//     *****
+//    *   *
+//   *   *
+//  *   *
+// *****
+
+//code above pattern in java
+
+public class pattern1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 4 -(i-1); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || i == 5 || j == 1 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
