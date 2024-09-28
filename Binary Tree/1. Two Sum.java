5807dd6210cd0c26432447bac90e49617606ee56
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-09-28 10:43:38 +0530

    2 sum brute force

--- Binary Tree/1. Two Sum.java
+++ Binary Tree/1. Two Sum.java
@@ -1,5 +1,29 @@
 // Solution 1 - Brute Force
+// c++
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = 0;
+        for(auto i:nums){
+            len++;
+        }
+        
+
+        for(int i=0;i<len-1;i++){
+            
+                for(int j=i+1;j<len;j++){
+                    if(nums[i] + nums[j] == target){
+                        
+                        return {i,j};
+                    }
+                }
+            
+        }
+        return {};
+
+    }
+};
 
 
 
