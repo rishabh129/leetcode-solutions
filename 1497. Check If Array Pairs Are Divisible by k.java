55a553e910be815049eace3199370bc26682a101
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-10-01 20:39:46 +0530

    freq array

---
+++ 1497. Check If Array Pairs Are Divisible by k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        int[] freq = new int[k];
+
+        for(int i:arr){
+            int rem = i%k;
+            if(rem<0){
+                rem+=k;
+            }
+            freq[rem]++;
+        }
+
+        if(freq[0]%2 != 0){
+            return false;
+        }
+
+        for(int i=1;i<k;i++){
+            if(freq[i]!=freq[k-i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
