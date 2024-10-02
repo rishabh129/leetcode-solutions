9be0df27198712ed6c3a94251c593767c49ef790
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-10-03 00:34:52 +0530

    majority element using map

--- 169_Majority_Element.cpp
+++ 169_Majority_Element.cpp
@@ -7,3 +7,33 @@ public:
         return nums[nums.size() / 2];
     }
 };
+
+// using Hashmap - Java
+
+class Solution
+{
+public
+    int majorityElement(int[] nums)
+    {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (freq.containsKey(nums[i]))
+            {
+                int count = freq.get(nums[i]);
+                freq.put(nums[i], count + 1);
+
+                if ((count + 1) > (nums.length / 2))
+                {
+                    return nums[i];
+                }
+            }
+            else
+            {
+                freq.put(nums[i], 1);
+            }
+        }
+        return nums[0];
+    }
+}
