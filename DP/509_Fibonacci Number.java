6505e125cd1eb3f6264dabd43d83395a7eb8b9dc
Rishabh Shankar <74086591+rishabh129@users.noreply.github.com>
2025-11-11 01:37:15 +0530

    DP: Memoization fib

--- DP/509_Fibonacci Number.java
+++ DP/509_Fibonacci Number.java
@@ -12,4 +12,29 @@ class Solution {
 
 
 // DP:  Memoization
+class Solution {
+
+    int f(int n, int[] dp){
+
+        if(n<=1)
+        return n;
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        return dp[n]=f(n-1, dp)+f(n-2, dp);
+    }
+    
+    public int fib(int n) {
+
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++)
+        dp[i]=-1;
+
+        return f(n, dp);
+
+    }
+}
+
+// DP: Tabulation
 
