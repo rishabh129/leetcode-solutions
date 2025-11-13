a46c4b8ec66f72934213ad8b88936ac5765d99fe
Rishabh Shankar <74086591+rishabh129@users.noreply.github.com>
2025-11-13 20:39:14 +0530

    Frog jump memoization

--- GFG/Frog Jump.java
+++ GFG/Frog Jump.java
@@ -16,3 +16,26 @@ class Solution {
         return f(height.length-1, height);
     }
 }
+
+//Memoization
+class Solution {
+    int f(int n, int[] ar, int[] dp){
+        if(n==0)
+        return 0;
+        if(dp[n]!=-1)
+        return dp[n];
+
+        int left = f(n-1, ar, dp) + Math.abs(ar[n]-ar[n-1]);
+        int right=Integer.MAX_VALUE;
+        if(n>1)
+        right = f(n-2, ar, dp) + Math.abs(ar[n]-ar[n-2]);
+        return dp[n]=Math.min(left, right);
+    }
+    int minCost(int[] height) {
+        // code here
+        int[] dp=new int[height.length+1];
+        for(int i=0;i<dp.length;i++)
+        dp[i]=-1;
+        return f(height.length-1, height, dp);
+    }
+}
