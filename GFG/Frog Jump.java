ddfc8d2ddca18857a87b90f8146f82853b50db60
Rishabh Shankar <74086591+rishabh129@users.noreply.github.com>
2025-11-13 22:36:43 +0530

    Frog jump tabulation

--- GFG/Frog Jump.java
+++ GFG/Frog Jump.java
@@ -39,3 +39,23 @@ class Solution {
         return f(height.length-1, height, dp);
     }
 }
+
+//Tabulation
+
+class Solution {
+    int minCost(int[] height) {
+        // code here
+        if(height.length==1)
+        return 0;
+          
+        int[] dp=new int[height.length+1];
+        dp[0]=0;
+        dp[1]=Math.abs(height[1]-height[0]);
+        
+        for(int i=2;i<height.length;i++){
+            dp[i] = Math.min(dp[i-1]+Math.abs(height[i]-height[i-1]), dp[i-2]+Math.abs(height[i]-height[i-2]));
+            
+        }
+        return dp[height.length-1];
+    }
+}
