a4ee24b7771e8360cf263d19044aec13492a96f7
Rishabh Shankar <74086591+rishabh129@users.noreply.github.com>
2025-11-17 22:36:34 +0530

    House robber tabulation

--- DP/198_House Robber.java
+++ DP/198_House Robber.java
@@ -41,3 +41,24 @@ class Solution {
         return f(nums.length-1, nums, dp);
     }
 }
+
+//Tabulation
+
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp, -1);
+        dp[0]=nums[0];
+
+        for(int i=1;i<n;i++){
+            int notPick=dp[i-1];
+            int pick=nums[i];
+            if(i>1)
+            pick+=dp[i-2];
+
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[n-1];
+    }
+}
