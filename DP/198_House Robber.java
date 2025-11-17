48d3954bab84ca6957fb964c0b78979a015b9702
Rishabh Shankar <74086591+rishabh129@users.noreply.github.com>
2025-11-17 22:14:35 +0530

    House robber memoization

--- DP/198_House Robber.java
+++ DP/198_House Robber.java
@@ -17,3 +17,27 @@ class Solution {
         return f(nums.length-1, nums);
     }
 }
+
+//Memoization
+
+class Solution {
+    int f(int n, int[] nums, int[] dp){
+        if(n==0)
+            return nums[n];
+        if(n<0)
+            return 0;
+
+        if(dp[n]!=-1)
+            return dp[n];
+
+        int pick=nums[n]+f(n-2, nums, dp);
+        int notPick=f(n-1, nums, dp);
+        return dp[n]=Math.max(pick, notPick);
+        
+    }
+    public int rob(int[] nums) {
+        int[] dp=new int[nums.length];
+        Arrays.fill(dp, -1);
+        return f(nums.length-1, nums, dp);
+    }
+}
